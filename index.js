d2c2122aaed2cd5e9eb147bf143e56dcd54e2caf
dariodlearnco <dario.dorella@gmail.com>
2018-01-08 03:23:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+    var theBeatles = []
+    for (var i=0; i < musicians.lenght; i++) {
+        beatles.push(`${musicians[i]} plays ${instruments}`)
+    }
+    return theBeatles
+}
