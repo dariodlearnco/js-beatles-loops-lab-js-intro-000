6f2ffdb24dc73fb78d3861e5569abfe6bb193227
dariodlearnco <dario.dorella@gmail.com>
2018-01-08 03:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
     var theBeatles = []
+    
     for (var i=0; i < musicians.lenght; i++) {
         theBeatles.push(i)
         theBeatles.push(`${musicians[i]} plays ${instruments[i]}`)
